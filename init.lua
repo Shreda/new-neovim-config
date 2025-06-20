531417321d7468768be90441a81fb78952555fd4
Daniel Moore <danielmoore.info@gmail.com>
2025-06-21 08:53:52 +1000

    color

--- init.lua
+++ init.lua
@@ -178,6 +178,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	end,
 })
 
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#51B3EC' })
 vim.api.nvim_set_hl(0, 'LineNr', { fg = 'white', bold = true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#FB508F' })
